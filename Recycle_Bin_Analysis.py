3c759bb0d680fc4b939c5b65a5a2bb51112da30f
Moses Arocha <carocha24@gmail.com>
2016-08-31 23:56:33 -0500

    Update

--- Recycle_Bin_Analysis.py
+++ Recycle_Bin_Analysis.py
@@ -13,7 +13,7 @@ def IDConverter(UserID):
         user = value.split('\\')[-1]	# Converts the users SID into readable names that the user can recongize.
         return user
     except:
-        return UserID			 	    # A catch all if a user's SID cannot be decoded, it just returns the SID.
+        return UserID			# A catch all if a user's SID cannot be decoded, it just returns the SID.
 
 def ReturnsDirectory():
     dirs=['C:\\Recycler\\','C:\\Recycled\\','C:\\$Recycle.Bin\\']	# Directs to each users recycle bin directories.
@@ -26,10 +26,10 @@ def findRecycled(RecycleBinDirectory):
     DirectoryListing = os.listdir(RecycleBinDirectory)
     for UserID in DirectoryListing:
         files = os.listdir(RecycleBinDirectory + UserID)	# Per user, it analyzes each directory.
-	user = IDConverter(UserID)			            	# References the IDConverter function, which converts the SID.
+	user = IDConverter(UserID)			        # References the IDConverter function, which converts the SID.
         print '\n\t[*] Listing Recycle Bin Files For User : ' + str(user) 
 	i = 0
-	for file in files:				                	# For loop that grabs each file within each recycle bin directory per user.
+	for file in files:				         # For loop that grabs each file within each recycle bin directory per user.
             i = i + 1				                    	
 	    print "[", i, "] Found File: " + str(file)
 
