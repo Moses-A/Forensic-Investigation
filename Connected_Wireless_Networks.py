2dc3fc339371ffc0308e6dcd9587a3dfe51ef0b0
Moses Arocha <carocha24@gmail.com>
2016-08-31 23:25:37 -0500

    Update

--- Connected_Wireless_Networks.py
+++ Connected_Wireless_Networks.py
@@ -4,36 +4,35 @@
 
 from _winreg import *
 
-def BinaryToMac(val):				# Converts the binary information from the Windows Registry to readable MAC Addresses
-	addr = '';
-	for ch in val:
-	   addr += '%02x '% ord(ch)		# Uses a binary to MAC conversion method, replaces spaces with ":" and returns the finished address
-	addr = addr.strip(' ').replace(' ', ':')[0:17]
-	return addr			
+def BinaryToMac(val):				# Converts the binary information from the Windows Registry to readable MAC Addresses.
+    addr = '';
+    for ch in val:
+       addr += '%02x '% ord(ch)		# Uses a binary to MAC conversion method, replaces spaces with ":" and returns the finished address
+    addr = addr.strip(' ').replace(' ', ':')[0:17]
+    return addr			
 
 def PrintNetworks():
-	net = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged" # The root location of the MAC Addresses
-	key = OpenKey(HKEY_LOCAL_MACHINE, net)
-	print '\n\t[Scanning] Wifi Networks Previously Joined\n'	#Out put the user finally sees, if reached, location search successful
-	for i in range(50):						# Repeats task 50 times, of grabbing the information from the WR
-	   try:
-		guid = EnumKey(key, i)
-		netKey = OpenKey(key, str(guid))
-		(n, addr, t) = EnumValue(netKey, 5)                     # Grabs the MAC Address of each connection
-		(n, name, t) = EnumValue(netKey, 4)                     # Grabs the name of each connection
-                (n, dns, t)  = EnumValue(netKey, 3)                     # Grabs the DNS server used of each connection
-		MACAddr = BinaryToMac(addr)				# After grabbing the info, sends it to the BinaryToMac function where the conversion occurs
-		netName = str(name)
-		DNServer = str(dns)
-		print '\n\t[Success] ' + netName + ' ' + MACAddr + '\n\t DNS Server : ' + DNServer # If successful, outputs this
-		CloseKey(netKey)
- 	   except:
-		break					# Finally ends with closing the file
+    net = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged" # The root location of the MAC Addresses
+    key = OpenKey(HKEY_LOCAL_MACHINE, net)
+    print '\n\t[Scanning] Wifi Networks Previously Joined\n'
+    for i in range(50):					
+        try:
+            guid = EnumKey(key, i)
+        	netKey = OpenKey(key, str(guid))
+        	(n, addr, t) = EnumValue(netKey, 5)     # Grabs the MAC Address of each connection
+        	(n, name, t) = EnumValue(netKey, 4)     # Grabs the name of each connection
+        	(n, dns, t)  = EnumValue(netKey, 3)     # Grabs the DNS server used of each connection
+        	MACAddr = BinaryToMac(addr)				# After grabbing the info, sends it to the BinaryToMac function where the conversion occurs
+        	netName = str(name)
+            DNServer = str(dns)
+    	    print '\n\t[Success] ' + netName + ' ' + MACAddr + '\n\t DNS Server : ' + DNServer 
+    	    CloseKey(netKey)
+        except:
+	        break
 
-##The main function, references the PrintNetworks function, which calls the BinaryToMac function##
 def main():
-	PrintNetworks()
+    PrintNetworks()
 	
 if __name__ == "__main__":
-	main()
-		
+    main()
+	
